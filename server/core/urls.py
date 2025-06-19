a02be0fda1d0069fbca59244714c535592f3d595
Dip <dipkorimon@gmail.com>
2025-06-19 06:07:55 +0600

    >> APP FIX<GenerateCode>: Changed url convention;

--- server/core/urls.py
+++ server/core/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/generate_code', include('generate_code.urls')),
+    path('api/generate_code/', include('generate_code.urls')),
     path('api/auth/', include('auth.urls')),
 ]
