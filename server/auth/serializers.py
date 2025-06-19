4640d15ea4fa3bff0e910dc3c04489d4919aa497
Dip <dipkorimon@gmail.com>
2025-06-20 05:23:13 +0600

    >> APP MOD<Auth>: Confirm password field created;

--- server/auth/serializers.py
+++ server/auth/serializers.py
@@ -3,10 +3,18 @@ from rest_framework import serializers
 
 
 class RegisterSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ['username', 'email', 'password']
+        fields = ['username', 'email', 'password', 'confirm_password']
         extra_kwargs = {'password': {'write_only': True}}
 
+    def validate(self, attrs):
+        if attrs['password'] != attrs['confirm_password']:
+            raise serializers.ValidationError('Passwords do not match')
+        return attrs
+
     def create(self, validated_data):
+        validated_data.pop('confirm_password')
         return User.objects.create_user(**validated_data)
