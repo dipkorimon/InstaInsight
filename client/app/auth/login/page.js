d4f37b5b50f0529ab69d48742db7a4248c9253c0
Dip <dipkorimon@gmail.com>
2025-06-21 22:47:44 +0600

    >> APP ADD<Auth>: Added login functionality;

--- client/app/auth/login/page.js
+++ client/app/auth/login/page.js
@@ -1,24 +1,83 @@
+"use client";
+
 import InputField from "@/components/InputField";
 import SubmitButton from "@/components/SubmitButton";
 import Link from "next/link";
+import {useRouter} from "next/navigation";
+import {useState} from "react";
 
-export default function RegisterPage() {
+export default function LoginPage() {
+    const router = useRouter();
+
+    const [form, setForm] = useState({
+        username: "",
+        password: "",
+    });
+
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState("");
+
+    const handleChange = (e) => {
+        setForm({ ...form, [e.target.name]: e.target.value });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setError("");
+        setSuccess("");
+
+        const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+        try {
+            const res = await fetch(`${API_BASE_URL}/api/auth/login/`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(form),
+            });
+
+            const data = await res.json();
+
+            if (res.ok) {
+                setSuccess(data.msg || "Login successful");
+                setError("");
+
+                setTimeout(() => {
+                    router.push("/");
+                }, 1500);
+            } else {
+                setError(data.error || "Invalid credentials");
+                setSuccess("");
+            }
+        } catch (err) {
+            setError("Server error");
+            setSuccess("");
+        }
+    };
+
     return (
         <div className="page-wrapper">
             <div className="card">
                 <h2 className="heading">Sign in to your account</h2>
+                {error && <p className="error-text">{error}</p>}
+                {success && <p className="success-text">{success}</p>}
 
-                <form className="space-y-3 mb-4">
+                <form onSubmit={handleSubmit} className="space-y-3 mb-4">
                     <InputField
                         name="username"
                         placeholder="Username"
                         label="Username"
+                        value={form.username}
+                        onChange={handleChange}
                     />
                     <InputField
                         name="password"
                         type="password"
                         placeholder="password"
                         label="Password"
+                        value={form.password}
+                        onChange={handleChange}
                     />
                     <div className="flex items-center justify-between">
                         <div className="flex items-start">
