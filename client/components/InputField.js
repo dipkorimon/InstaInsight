153da93c2b7f8e678ac54283a4f98aa7b6061c89
Dip <dipkorimon@gmail.com>
2025-06-27 05:18:13 +0600

    >> APP MOD<Components>: - Icon added in InputField component;

--- client/components/InputField.js
+++ client/components/InputField.js
@@ -1,18 +1,29 @@
 "use client";
 
-export default function InputField({ name, type = "text", value, onChange, placeholder, label}) {
+export default function InputField({ name, type = "text", value, onChange, placeholder, label, icon: Icon }) {
     return (
         <div className="input-field mb-4">
-            <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900">{label}</label>
-            <input
-                type={type}
-                name={name}
-                value={value}
-                onChange={onChange}
-                placeholder={placeholder}
-                className="w-full border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-100"
-                required
-            />
+            <label htmlFor={name} className="block mb-2 text-sm font-medium text-gray-900">
+                {label}
+            </label>
+            <div className="relative">
+                {Icon && (
+                    <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-400">
+                        <Icon className="text-lg" />
+                    </span>
+                )}
+                <input
+                    type={type}
+                    name={name}
+                    value={value}
+                    onChange={onChange}
+                    placeholder={placeholder}
+                    className={`w-full border border-gray-300 p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-100 ${
+                        Icon ? "pl-10" : ""
+                    }`}
+                    required
+                />
+            </div>
         </div>
     );
 }
