3fcf8b622754d74bd7bb52e8e89b5ce27a5c368a
Dip <dipkorimon@gmail.com>
2025-06-27 05:04:48 +0600

    >> APP FIX<Auth>: - Some text changed in login page;

--- client/app/auth/login/page.js
+++ client/app/auth/login/page.js
@@ -78,7 +78,7 @@ export default function LoginPage() {
                     <InputField
                         name="password"
                         type="password"
-                        placeholder="password"
+                        placeholder="Password"
                         label="Password"
                         value={form.password}
                         onChange={handleChange}
