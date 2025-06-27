1f10398979752d67286a2e07d4404d4dcc974ac1
Dip <dipkorimon@gmail.com>
2025-06-27 23:23:38 +0600

    >> APP MOD<Components>: - Update the users API;

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -31,7 +31,7 @@ export default function Navbar() {
                 const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 
                 try {
-                    const res = await fetch(`${API_BASE_URL}/api/user-info/`, {
+                    const res = await fetch(`${API_BASE_URL}/api/users/user-info/`, {
                         headers: {
                             Authorization: `Token ${token}`,
                         },
